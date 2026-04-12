8b89fb0285e963a2cc60e2f0d7a7be726e100172
Cole Fang <1875161218@qq.com>
2026-04-12 13:39:46 +0800

    📰 2026-04-12 13:39:45 自动备份

---
+++ open-vault.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# 打开 Obsidian 并自动进入 tech-news-digest vault
+open -a Obsidian --args --vault "/Users/ryanfong/Documents/tech-news-digest"
+echo "✅ Obsidian 已启动，正在打开 tech-news-digest vault..."
